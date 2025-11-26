212f0642c9f8fefe6f7b23690357b511823a849d
vyshreddy <bodduvyshnavi77@gmail.com>
2025-11-26 07:01:31 +0530

    Added SwapNumbers program

---
+++ programs/SwapNumbers.java
@@ -0,0 +1,22 @@
+package programs;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before Swap:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // swap without third variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swap:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
